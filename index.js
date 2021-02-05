877cbaa169b9b0cc81c7913ce1a444e5eece21c9
Todd Goodall <todd@torotms.com>
2021-02-05 12:46:26 -0800

    extract seed orders

--- index.js
+++ index.js
@@ -2,27 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { nanoid } = require("nanoid");
 
-const PORT = 3000;
+const orders = require("./orders");
 
-const orders = [
-  {
-    id: nanoid(5),
-    customer: "CH Robinson",
-    pickup: {
-      address: "123 Main St. San Diego, CA 92109",
-      time: new Date("Feb 7 2021 6:00 am PST"),
-    },
-    dropoff: {
-      address: "123 Second St. Oxnard, CA 98249",
-      time: new Date("Feb 10 2021 8:00 am PST"),
-    },
-    weight: 34000,
-    rateCents: 120000,
-  },
-];
+const PORT = 3000;
 
 const app = express();
 app.use(bodyParser.json());
+
+const exampleId = nanoid(5);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -30,4 +17,5 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`app listening at http://localhost:${PORT}`);
+  console.log("seed orders: ", orders);
 });
---
+++ orders.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    id: "L_RBr",
+    customer: "CH Robinson",
+    pickupAddress: "123 Main St.",
+    dropoff: "123 Second St.",
+    weight: 34000,
+    rateCents: 120000,
+  },
+];
