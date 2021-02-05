32f3fd834f494c694734beeaa133a035ee164e51
Todd Goodall <todd@torotms.com>
2021-02-05 11:54:19 -0800

    create hello world route and dummy order

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+const express = require("express");
+const { nanoid } = require("nanoid");
+
+const orders = [
+  {
+    id: nanoid(5),
+    customer: "CH Robinson",
+    pickup: {
+      address: "123 Main St. San Diego, CA 92109",
+      time: new Date("Feb 7 2021 6:00 am"),
+    },
+    dropoff: {
+      address: "123 Second St. Oxnard, CA 98249",
+      time: new Date("Feb 10 2021 8:00 am"),
+    },
+    weight: 34000,
+    rateCents: 120000,
+  },
+];
+
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
